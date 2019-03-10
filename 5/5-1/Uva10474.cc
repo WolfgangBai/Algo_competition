33090f4f06fc6ec664e28935b4905226e65d0364
WolfgangBai <928991159@qq.com>
2019-03-10 22:51:53 +0800

    5-1

---
+++ 5/5-1/Uva10474.cc
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int max_n = 10000;
+
+int main() 
+{
+    int n,q,x,a[max_n],kase = 0;
+    while (scanf("%d%d", &n, &q) == 2 && n) {
+        printf("CASE# %d:\n", ++kase);
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        sort(a, a + n);
+        while (q--) {
+            scanf("%d", &x);
+            int p = lower_bound(a, a + n, x) - a;
+            if (a[p] == x) {
+                printf ("%d found at %d\n", x, p + 1);
+            } else {
+                printf("%d not found\n", x);
+            }
+        }
+    }
+}
+
